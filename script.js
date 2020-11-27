966e87248257fe5b62f18a6bcb53d7fb1f6838fb
tomwhiite <twhi7149@uni.sydney.edu.au>
2020-11-27 06:18:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+const setActive = (el, active) => {
+  const formField = el.parentNode.parentNode
+  if (active) {
+    formField.classList.add('form-field--is-active')
+  } else {
+    formField.classList.remove('form-field--is-active')
+    el.value === '' ? 
+      formField.classList.remove('form-field--is-filled') : 
+      formField.classList.add('form-field--is-filled')
+  }
+}
+
+[].forEach.call(
+  document.querySelectorAll('.form-field__input, .form-field__textarea'),
+  (el) => {
+    el.onblur = () => {
+      setActive(el, false)
+    }
+    el.onfocus = () => {
+      setActive(el, true)
+    }
+  }
+)
